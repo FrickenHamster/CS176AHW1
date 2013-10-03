dcf1236e48828a418bf09937a0258f51c6a977a2
Fricken Hamster <frickenhamster@gmail.com>
2013-10-03 11:32:15 -0700

    最初のプーシー

---
+++ src/client_java_tcp.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fricken Hamster
+ * Date: 10/3/13
+ * Time: 11:30 AM
+ */
+public class client_java_tcp
+{
+
+	public client_java_tcp()
+	{
+
+	}
+}
---
+++ src/server_java_tcp.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fricken Hamster
+ * Date: 10/3/13
+ * Time: 11:31 AM
+ */
+public class server_java_tcp
+{
+
+	public server_java_tcp()
+	{
+
+	}
+}
